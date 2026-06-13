3d0b80fe04697f4ff741ba628ad7e3b5e301d18f
Tomiwa200 <bankolefemi09@gmail.com>
2026-06-13 16:04:06 +0100

    admin page deloyment error

--- src/features/admin/components/AnalyticsDashboardWithNoSSR.tsx
+++ src/features/admin/components/AnalyticsDashboardWithNoSSR.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 
   const BarChart = dynamic(
@@ -15,6 +16,9 @@ import dynamic from "next/dynamic";
   })
 
   export default function AnalyticsDashboardWithNoSSR() {
-    return <BarChart />
+    return 
+    <Suspense fallback={<div>Loading dashboard metrics...</div>}>
+      <BarChart />
+    </Suspense>
   }
   
